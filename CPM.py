5b8c350658eca28710df31654a9984eedeb3e9c0
MiguelAguilera <miguel@maguilera.net>
2017-08-11 11:11:07 +0200

    add connectivity algorithm

--- CPM.py
+++ CPM.py
@@ -29,9 +29,12 @@ class CPM:
 		self.initialize_state()
 		self.set_temp()
 		
+		
 	def set_temp(self,T=1):
 		self.T=1.0
 		self.beta=1.0/self.T
+		
+		
 	def set_beta(self,B):
 		self.beta=B
 		if B==0:
@@ -53,24 +56,24 @@ class CPM:
 #		self.s = np.random.randint(0,self.c,(self.L,self.L))
 		self.s=np.zeros((self.L,self.L),int)	#add starting cells in random positions
 		for ind in range(1,self.c):
-			i,j = np.random.randint(0,self.L,2)		#row,column
-			self.s[i-1:i+2,j]=ind
-			self.s[i,j-1:j+2]=ind
+			i,j = np.random.randint(1,self.L-1,2)	# row,column of cell, borders are forbidden
+			self.s[i,j]=ind
 		self.VE = self.volume_energy(self.s)
 
 
 	def volume_energy(self,s):
 		unique, counts = np.unique(s, return_counts=True)
 		V=counts[1:]
-		return np.sum((V-self.V0)**2)
+		return self.th/(2*self.V0)*np.sum((V-self.V0)**2)
 		
+		#old code computing areas of connected blobs
 #		cells= measure.label(s,connectivity=1)
 #		props=measure.regionprops(cells)
 #		print('areas',[prop.filled_area for prop in props])
-#		return np.sum([(prop.filled_area-self.V0)**2 for prop in props])	#areas of connected blobs
+#		return np.sum([(prop.filled_area-self.V0)**2 for prop in props])
 
 
-	def get_neighbors(self,i,j):	# Get the array of neighbors of a cell
+	def get_neighbors(self,i,j):	# Get the array of Von Newmann neighbors of a cell
 		nn = []
 		nn+=[self.s[(i+1)%self.L,j]]	#up
 		nn+=[self.s[(i-1)%self.L,j]]	#down
@@ -80,41 +83,79 @@ class CPM:
 		return np.array(nn)
 			
 			
-	def ModifiedMetropolisStep(self):	    # Execute step of Metropolis algorithm
+	def get_moore_neighbors(self,i,j):	# Get the array of Moore neighbors of a cell
+		nn = []
+		nn+=[self.s[(i+1)%self.L,(j-1)%self.L]]	#up-left
+		nn+=[self.s[(i+1)%self.L,j]]			#up
+		nn+=[self.s[(i+1)%self.L,(j+1)%self.L]]	#up-right
+		nn+=[self.s[i,(j+1)%self.L]]			#right
+		nn+=[self.s[(i-1)%self.L,(j+1)%self.L]]	#down-right
+		nn+=[self.s[(i-1)%self.L,j]]			#down
+		nn+=[self.s[(i-1)%self.L,(j-1)%self.L]]	#down-left
+		nn+=[self.s[i,(j-1)%self.L]]			#left
+		return np.array(nn)	
 		
-		#Select target node
-		i,j = np.random.randint(0,self.L,2)		# row,column of cell
-		nn=self.get_neighbors(i,j)				# array of cell neighbors 
-		while np.sum(np.abs(self.s[i,j]-nn))==0:
-			i,j = np.random.randint(0,self.L,2)	
-			nn=self.get_neighbors(i,j)
-		notequal=np.where(nn!=self.s[i,j])[0]	# neighbors different from cell
-		sijnew = nn[notequal[np.random.randint(len(notequal))]]		# target state
+		
+	def is_locally_connected(self,cell_domain): # Detect local connectivity
+		# cell_domain should be 1 if cell is present and 0 otherwise
+		transitions=0
+		is_connected=False
+		if np.sum(cell_domain)>0:
+			for i in range(8):
+				if cell_domain[i]<cell_domain[(i+1)%8]:	# +1 if transition from 0 to 1
+					transitions+=1
+			if transitions<=1:
+				is_connected=True
+		return is_connected
 
-		eDiff = 0
+
+
+	def MetropolisStep(self,mode='CA'):	    # Execute step of Metropolis algorithm
 		
-		#Compute adhesion energy difference
-		for sn in nn:
-			eDiff+= self.J[self.type[sijnew],self.type[sn]]*int(sijnew!=sn) - self.J[self.type[self.s[i,j]],self.type[sn]]*int(self.s[i,j]!=sn) 
-		
-		#Compute volume energy difference
-		snew=self.s.copy()
-		snew[i,j] = sijnew
-		VEnew = self.volume_energy(snew)
-		eDiff += VEnew - self.VE
-		
-		if eDiff <= 0 or np.random.rand() < np.exp(-self.beta*eDiff):    # Metropolis
-			self.s[i,j] = sijnew
-			self.VE = VEnew
+		#Select candidate and target nodes	
+		if mode=='MMA':
+			i,j = np.random.randint(1,self.L-1,2)	# row,column of cell, borders are forbidden
+			nn=self.get_neighbors(i,j)				# array of cell neighbors 
+			sijnew = nn[np.random.randint(len(nn))]		# target state
+			cond = sijnew!=self.s[i,j]
+			
+		if mode=='CA':
+			i,j = np.random.randint(1,self.L-1,2)	# row,column of cell
+			nn=self.get_neighbors(i,j)				# array of cell neighbors 
+			nn_unique=np.unique(nn)
+			sijnew = nn_unique[np.random.randint(len(nn_unique))]		# target state
+			domain=self.get_moore_neighbors(i,j)
+			lc_candidate=self.is_locally_connected(domain==self.s[i,j])
+			lc_target=self.is_locally_connected(domain==sijnew)
+			cond = lc_candidate and lc_target and sijnew!=self.s[i,j]
+
+			
+		#Evaluate acceptance of change
+		if cond:
+			eDiff = 0
+			
+			coupling_neighbors=self.get_neighbors(i,j)
+			#Compute adhesion energy difference
+			for sn in coupling_neighbors:
+				eDiff+= self.J[self.type[sijnew],self.type[sn]]*int(sijnew!=sn) - self.J[self.type[self.s[i,j]],self.type[sn]]*int(self.s[i,j]!=sn) 
+			#Compute volume energy difference
+			snew=self.s.copy()
+			snew[i,j] = sijnew
+			VEnew = self.volume_energy(snew)
+			eDiff += VEnew - self.VE
+			if eDiff <= 0 or np.log(np.random.rand()) < -self.beta*eDiff:    # Metropolis
+				self.s[i,j] = sijnew
+				self.VE = VEnew
 				
 
 	def Energy(self):
-		E=self.VE
+		E=self.volume_energy(self.s)
 		for i in range(self.L):
 			for j in range(self.L):
-				E-= self.h[self.s[i,j]]
-				E-= self.J*int(self.s[i,j]==self.s[(i+1)%self.L,j])	#up
-				E-= self.J*int(self.s[i,j]==self.s[(i-1)%self.L,j])	#down
-				E-= self.J*int(self.s[i,j]==self.s[i,(j+1)%self.L])	#right
-				E-= self.J*int(self.s[i,j]==self.s[i,(j-1)%self.L])	#left
+				if self.s[i,j]>0:
+					coupling_neighbors=self.get_neighbors(i,j)
+					#Compute adhesion energy difference
+					for sn in coupling_neighbors:
+						if sn<self.s[i,j]:	#we compute each link just once
+							E+= self.J[self.type[self.s[i,j]],self.type[sn]]*int(self.s[i,j]!=sn)
 		return(E)
